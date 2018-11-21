f35b83a632af359e8ade0c8cee8e24abe6c60962
Jeffrey Cegan <jeffrey.c.cegan@usace.army.mil>
2018-11-21 09:26:58 -0500

    file setup

---
+++ data-wrangle.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+setwd("C:/Users/xadmin/projects/state-department")
+example <- read.csv("data/example.csv")
+#murders <- murders %>% mutate(regions = factor(region), rate = total / population * 10^5)
+head(example)
+
+#save(example,file = "rda/example.rda")
+
