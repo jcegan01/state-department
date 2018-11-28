6e803ba0297798598d2dd700e319438ed7d00463
Jeffrey Cegan <jeffrey.c.cegan@usace.army.mil>
2018-11-28 12:54:11 -0500

    updated rename

--- data-wrangle.R
+++ data-wrangle.R
@@ -1,8 +1,22 @@
 library(tidyverse)
+library(reshape2)
+library(plyr)
+
 setwd("C:/Users/xadmin/projects/state-department")
 example <- read.csv("data/example.csv")
-#murders <- murders %>% mutate(regions = factor(region), rate = total / population * 10^5)
 head(example)
+
+psm <- read.csv("data/physical-security-measures.csv")
+head(psm)
+
+tl <- read.csv("data/threat-level.csv")
+tl <- melt(tl)
+tl <- rename(tl, c("X"="type", "variable"="level"))
+ 
+
+
+#murders <- murders %>% mutate(regions = factor(region), rate = total / population * 10^5)
+
 
 #save(example,file = "rda/example.rda")
 
